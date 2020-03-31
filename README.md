b2ce22e85614d3cb5bc3cfd9f22acfa6ad755862
Gretel Gutierrez <gretelg@3523235.no-reply.drupal.org>
2020-03-31 07:35:59 -0600

    Use react to list content from Drupal

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 1. Enable the theme React -- Simple Hello World.
-2. Override the node template, on templates folder.
-3. Add a code to load react app. <div id="react-app"><div>
-4. Create an Article or a Page content type node.
-5. Create a custom react script in js folder.
-6. Add react libraries in react_simple.libraries.yml file.
-7. Load react libraries in node.html.twig file.
-8. View the node on the browser. See that react is loaded.
-9. Create 2 components in js file to show or hide content from a node.
+2. Add react libraries in react_simple.libraries.yml file.
+3. Use a preprocess function to pass the title and the nid to the template.
+4. Override the node template, on templates folder.
+5. Add a code to load react app. <div id="react-app"><div> with data.* attributes.
+6. Load react libraries in node.html.twig file.
+7. Create some Articles.
+8. Create a custom react script in js folder.
+9. View any node on the browser. See that react is loaded.
--- js/index.js
+++ js/index.js
@@ -1,91 +1,120 @@
-// React & ReactDOM are loaded by react_simple.libraries.yml
+// Called once for each node returned from JSON:API
+class NodeItem extends React.Component {
+  render() {
+    return <div>
+      <h2>{this.props.attributes.title}</h2>
+      {this.props.attributes.body && <div dangerouslySetInnerHTML={{__html: this.props.attributes.body.value}} />}
+    </div>;
+  }
+}
 
-// Shows a button that toggles the display of the node content.
-// dangerouslySetInnerHTML is React's replacement for using innerHTML in the browser DOM.
-class ShowNodeContent extends React.Component {
+class NoData extends React.Component {
+  render() {
+    return <div>No articles found.</div>;
+  }
+}
+
+class NodeList extends React.Component {
+  // Initialize the this.state variable
   constructor() {
     super();
-    this.state = {
-      status: 0,
-    };
-    this.showContent = this.showContent.bind(this);
-    this.hideContent = this.hideContent.bind(this);
+    this.state = { data: null };
+    this.loadNodeData = this.loadNodeData.bind(this);
+    this.updateData = this.updateData.bind(this);
+    this.checkInvalidData = this.checkInvalidData.bind(this);
   }
 
-  showContent() {
-    this.setState({status: 1});
+  // When the component first loads, it will run loadNodeData()
+  componentWillMount() {
+    this.loadNodeData();
   }
-  hideContent() {
-    this.setState({status: 0});
+
+  loadNodeData() {
+    // Point to local Drupal instance.
+    const API_ROOT = 'https://my-drupal8-site.ddev.site:8300/jsonapi/';
+    const url = `${API_ROOT}node/article`;
+
+    // Retrieve data from Drupal's API and sends it to updateData()
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then((data) => this.updateData(data))
+      .catch(err => console.log('API got an error', err))
+  }
+
+  updateData(responseData) {
+    const validatedData = this.checkInvalidData(responseData);
+    console.log('validatedData: ', validatedData);
+
+    // Data is being retrieved
+    if (validatedData) {
+      this.setState( { data: responseData.data }, () => console.log('state', this.state));
+    }
+  }
+
+  // Validator function
+  checkInvalidData(data) {
+    if (data === null) {
+      return false;
+    }
+    if (data.data === undefined ||
+        data.data === null ||
+        data.data.length === 0 ) {
+      return false;
+    }
+    return true;
   }
 
   render() {
-    var style = {
-      active: {
-        width: '200px',
-        height: '40px',
-        padding: '10px',
+    // Add css to identify the current node
+    const style = {
+      nodeItem: {
+        border: '2px solid black',
         margin: '10px',
-        color: '#333',
-        backgroundColor: '#ddd',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignSelf: 'center',
-      },
-      inactive: {
-        width: '200px',
-        height: '40px',
         padding: '10px',
-        margin: '10px',
-        color: '#666',
         backgroundColor: '#ccc',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignSelf: 'center',
       },
-      content: {
-        margin: '30px',
-        padding: '30px',
-        backgroundColor: '#eee',
-        border: '5px solid black'
+      nodeItemActive: {
+        border: '3px solid red',
+        margin: '10px',
+        padding: '10px',
+        backgroundColor: '#fff',
       }
     };
 
     return (
       <div>
-        {this.state.status === 1 &&
-          <div>
-            <button onClick={this.hideContent} style={style.inactive}>Hide content</button>
-            <div style={style.content} dangerouslySetInnerHTML={{__html: this.props.content }}/>
-          </div>
-        }
-        {this.state.status === 0 &&
-          <button onClick={this.showContent} style={style.active}>Show content</button>
-        }
-     </div>
-    );
-  }
-}
+        <h2>Content list</h2>
 
-// Create a new component that displays the content of the node.
-class NodeContent extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <ShowNodeContent {...this.props} />
+        {this.state.data !== null ?
+          this.state.data.map(item => {
+            let className = 'nodeItem';
+            let current = false;
+
+            console.log('Number(this.props.nid): ', Number(this.props.nid));
+            console.log('Number(item.attributes.nid): ', Number(item.attributes.drupal_internal__nid));
+
+            if (Number(this.props.nid) === Number(item.attributes.drupal_internal__nid)) {
+              className = 'nodeItemActive';
+              current = true;
+            }
+
+            console.log(className);
+            return (<div style={style[className]}> <NodeItem current={current} {...item} /> </div>);
+          } )
+          :
+          <NoData />
+        }
       </div>
     );
   }
 }
 
+// data-nid is retrieved and passed as a prop to the NodeList component.
 ReactDOM.render(
-    // Connect component to virtual DOM
-    <NodeContent
-      title={document.getElementById('react-content').getAttribute("data-title")}
-      content={document.getElementById('react-content').innerHTML}
-    />,
-    document.getElementById('react-app')
+  <NodeList
+    nid = { document.getElementById('react-app').getAttribute('data-nid') }
+  />,
+  document.getElementById('react-app')
 );
